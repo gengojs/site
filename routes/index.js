bd6862186f9398f042a846287f775ed0a8b9acc4
iwatakeshi <iwatakeshi@gmail.comS>
2014-09-30 20:01:55 -0600

    fixed issue with docs v0.2 not appearing

--- routes/index.js
+++ routes/index.js
@@ -3,30 +3,29 @@ var router = express.Router();
 var gengo = require('gengojs');
 var keys = require('../views/docs/v0.2/gengokeys.js');
 var nativelang = {
-    "Japanese": "日本語"
+  "Japanese": "日本語"
 };
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', {
-        title: 'Gengo.js',
-        language: nativelang[gengo.language()] || gengo.language(),
-    });
+  res.render('index', {
+    title: 'Gengo.js',
+    language: nativelang[gengo.language()] || gengo.language(),
+  });
 });
 
 router.get('/docs', function(req, res) {
-    res.render('docs', {
-        title: 'Gengo.js',
-        language: nativelang[gengo.language()] || gengo.language(),
-    });
+  res.render('docs', {
+    title: 'Gengo.js',
+    language: nativelang[gengo.language()] || gengo.language(),
+  });
 });
 
 router.get('/docs/v0.2', function(req, res) {
-    console.log(gengo.locale());
-    res.render('docs/v0.2/docs', {
-        title: 'Gengo.js',
-        language: nativelang[gengo.language()] || gengo.language(),
-        keys: keys
-    });
+  res.render('docs/v0.2/docs', {
+    title: 'Gengo.js',
+    language: nativelang[gengo.language()] || gengo.language(),
+    keys: keys
+  });
 });
 
 module.exports = router;
