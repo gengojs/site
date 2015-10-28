d05768811d6c8be7e1c6f63ad5603abba64dc3df
iwatakeshi <iwatakeshi@gmail.com>
2015-10-28 16:28:23 -0600

    Update
    Updated docs.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 var gulp 		= require('gulp'),
 		rimraf 	= require('rimraf'),
-		git 		= require('gulp-git');
+		git 		= require('gulp-git'),
+		del			= require('del');
 
-gulp.task('git:clone', function(){
-	git.clone('https://github.com/gengojs/docs/', function (err) {});
+
+/**
+ * Task: Clone
+ */
+gulp.task('git:clone', function(cb){
+	git.clone('https://github.com/gengojs/docs/', function (error) {
+		cb();
+	});
 });
 
+/**
+ * Task: Move
+ */
 gulp.task('move:docs:html',['git:clone'], function(){
 	return gulp.src('docs/dist/html/*.ejs')
 	.pipe(gulp.dest('views/partials/'));
@@ -16,7 +26,15 @@ gulp.task('move:docs:json',['git:clone'], function(){
 	.pipe(gulp.dest('locales/'));
 });
 
+
+/**
+ * Task: Clean
+ */
 gulp.task('clean:docs', function(cb){
+	rimraf('docs/',cb);
+});
+
+gulp.task('clean:docs:clone',['git:clone'], function(cb){
 	rimraf('docs/',cb);
 });
 
@@ -24,6 +42,14 @@ gulp.task('clean:locales', function(cb){
 	rimraf('locales/',cb);
 });
 
-gulp.task('default', ['git:clone', 'move:docs:html', 'move:docs:json']);
+gulp.task('clean:templates', function(){
+	var views = 'views/partials/';
+	return del([
+		views + 'api.ejs',
+		views + 'intro.ejs'
+		]);
+});
 
-gulp.task('clean', ['clean:docs', 'clean:locales']);
+gulp.task('default', ['git:clone', 'move:docs:html', 'move:docs:json', 'clean:docs:clone']);
+
+gulp.task('clean', ['clean:docs', 'clean:locales', 'clean:docs']);
